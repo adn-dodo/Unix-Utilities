1c6417b319308d9619ab07494663a99c4bf5f35a
adn-dodo <elkawasadn@gmail.com>
2026-07-29 04:46:26 +0300

    MicroShell.c

--- MicroShell.c
+++ MicroShell.c
@@ -1,18 +1,11 @@
-/*
-still did not implemet the I/O
-just did not like it to be empty
-
-will update when finished
-ان شاء الله
-
-*/
-
-
-
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <dirent.h>
+
 #define count 8000
 #define max_vars 100
 
@@ -103,6 +96,102 @@ int main(int argc, char **argv)
         } else if (strcmp(buf, "exit") == 0) {
             printf("Good Bye :)\n");
             return 0;
+        } else if (strncmp(buf, "ls", 2) == 0) {
+            char cmd[count];
+            strcpy(cmd, buf);
+
+            for (int m = 0; m < var_count; m++) {
+                char search[600];
+                sprintf(search, "$%s", vars.names[m]);
+                char *pos = strstr(cmd, search);
+
+                if (pos != NULL) {
+                    char temp[count];
+                    int offset = pos - cmd;
+                    strncpy(temp, cmd, offset);
+                    temp[offset] = 0;
+                    strcat(temp, vars.values[m]);
+                    strcat(temp, pos + strlen(search));
+                    strcpy(cmd, temp);
+                }
+            }
+
+            if (strchr(cmd, '$') != NULL) {
+                printf("not there\n");
+            } else {
+                char path[500] = ".";
+                if (strlen(cmd) > 2) {
+                    if (cmd[2] == ' ') {
+                        strcpy(path, cmd + 3);
+                    }
+                }
+
+                DIR *dir = opendir(path);
+                if (dir == NULL) {
+                    printf("ls: cannot access '%s': No such file or directory\n", path);
+                } else {
+                    struct dirent *entry;
+                    while ((entry = readdir(dir)) != NULL) {
+                        if (entry->d_name[0] != '.') {
+                            printf("%s\n", entry->d_name);
+                        }
+                    }
+                    closedir(dir);
+                }
+            }
+        } else if (strchr(buf, '<') || strchr(buf, '>') || strstr(buf, "2>")) {
+            char cmd_copy[count];
+            strcpy(cmd_copy, buf);
+
+            char *input_file = NULL;
+            char *output_file = NULL;
+            char *error_file = NULL;
+
+            if ((strstr(cmd_copy, "2>")) != NULL) {
+                char *ptr = strstr(cmd_copy, "2>");
+                *ptr = '\0';
+                error_file = ptr + 2;
+                while (*error_file == ' ') error_file++;
+            }
+
+            if ((strchr(cmd_copy, '>')) != NULL) {
+                char *ptr = strchr(cmd_copy, '>');
+                *ptr = '\0';
+                output_file = ptr + 1;
+                while (*output_file == ' ') output_file++;
+            }
+            pid_t pid = fork();
+            if (pid == 0) {
+                if (input_file) {
+                    int fd_in = open(input_file, O_RDONLY);
+                    dup2(fd_in, STDIN_FILENO);
+                    close(fd_in);
+                }
+                if (output_file) {
+                    int fd_out = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                    dup2(fd_out, STDOUT_FILENO);
+                    close(fd_out);
+                }
+                if (error_file) {
+                    int fd_err = open(error_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                    dup2(fd_err, STDERR_FILENO);
+                    close(fd_err);
+                }
+
+                char *args[100];
+                int argc = 0;
+                char *token = strtok(cmd_copy, " ");
+                while (token != NULL) {
+                    args[argc++] = token;
+                    token = strtok(NULL, " ");
+                }
+                args[argc] = NULL;
+
+                execvp(args[0], args);
+                exit(-1);
+            } else {
+                wait(NULL);
+            }
         } else {
             printf("Invalid command\n");
         }
